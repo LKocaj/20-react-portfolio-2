e1c081a5da3f4ea5efe7264a966431f561dc235e
Lawrence <lawrencecaj@gmail.com>
2022-08-24 15:39:39 -0400

    fixed links

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,8 +5,11 @@ export default function Home() {
     <div className = "w-50 mx-auto">
       <h1 className ="pb-4 pt-2">About me</h1>
       <article>
-            <img class="float" src={"../assets/------.jpg"} alt="Lawrence himself"/>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            <img class="float" src={"./public/assets/Profile-pic.jpg"} alt="Lawrence himself"/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
         </article>
     </div>
   );
