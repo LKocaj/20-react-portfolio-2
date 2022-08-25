2a8ff8e576834be04cca4ba2a5e9f547ca2b02f0
Lawrence <lawrencecaj@gmail.com>
2022-08-25 14:32:27 -0400

    clean

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -8,9 +8,8 @@ const styles = {
 
 export default function PortfolioContainer({renderPage}) {
 
-
   return (
-    <div class="vh-100" style={styles.pageHeightStyle}>
+    <div class="vh-100 containd" style={styles.pageHeightStyle}>
       {renderPage()}
     </div>
   );
